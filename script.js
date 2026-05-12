0b8f9bdd7639e3f44821545c5c80500c120697cc
nanokamikaze-afk <nanokamikaze@outlook.es>
2026-05-11 20:57:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector(".nav-toggle");
+const mainNav = document.querySelector(".main-nav");
+
+if (navToggle && mainNav) {
+  navToggle.addEventListener("click", () => {
+    const isOpen = mainNav.classList.toggle("open");
+    navToggle.setAttribute("aria-expanded", String(isOpen));
+  });
+}
